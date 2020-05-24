88a1de88c421c3054884a13e8390516019a5b153
tjeubaoit <anhtn.bk@gmail.com>
2020-05-24 21:38:48 +0700

    leetcode 977

---
+++ leetcode/977-squares-of-a-sorted-array/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static int sqr(int n) {
+        return (int) Math.pow(n, 2);
+    }
+    
+    public int[] sortedSquares(int[] nums) {
+        int i = 0;
+        while (i < nums.length && nums[i] < 0) i++;
+        
+        int j = i--, k = 0;
+        int[] ans = new int[nums.length];
+        // System.out.println(i + " " + j);
+        
+        while (i >= 0 && j < nums.length) {
+            if (sqr(nums[i]) > sqr(nums[j])) {
+                ans[k++] = sqr(nums[j]);
+                j += 1;
+            } else {
+                ans[k++] = sqr(nums[i]);
+                i -= 1;
+            }
+        }
+        while (i >= 0) 
+            ans[k++] = sqr(nums[i--]);
+        while (j < nums.length) 
+            ans[k++] = sqr(nums[j++]);
+        
+        return ans;
+    }
+}
