0d5291f82beb6bea6920a1bd7b0b7c9639e4421f
tjeubaoit <anhtn.bk@gmail.com>
2020-07-17 09:53:12 +0700

    leetcode 135

--- leetcode/135.candy.py
+++ leetcode/135.candy.py
@@ -4,20 +4,22 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        ans = [1]*len(ratings)
-        for i in range(1, len(ratings)):
-            if ratings[i-1] < ratings[i]:
-                ans[i] = ans[i-1] + 1
-            else:
-                j = i
-                while j > 0 and ratings[j-1] > ratings[j] and ans[j-1] <= ans[j]:
-                    ans[j-1] = ans[j] + 1
-                    j -= 1
-        print(ans)
-        return sum(ans)
+        st = []
+        candies = [0]*len(ratings)
+        for i in range(len(ratings)):
+            if not st or st[-1] >= ratings[i]:
+                st.append(ratings[i])
+                continue
+            
+            while st:
+                j = st.pop()
+                candies[j] = candies[j+1] + 1
+        
+        print(candies)
+        return sum(candies)
 
 
 if __name__ == '__main__':
-    nums = [1, 2, 2]
+    nums = [1, 0, 2]
     ret = Solution().candy(nums)
     print(ret)        
