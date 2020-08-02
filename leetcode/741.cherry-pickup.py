ca32d62fe60cc3c9db9c9e4ecba7e3f18ac0f6ae
tjeubaoit <anhtn.bk@gmail.com>
2020-08-02 22:43:17 +0700

    leetcode 741

---
+++ leetcode/741.cherry-pickup.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=741 lang=python3
+#
+# [741] Cherry Pickup
+#
+import unittest
+from common import BaseTestCase
+from typing import List
+
+# @lc code=start
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+         
+        
+# @lc code=end
+class P741TestCase(BaseTestCase):
+
+    def test_1():
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
