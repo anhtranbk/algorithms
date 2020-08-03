f8a7abc0d36816cbfb616882fe721c4ca5038f6a
tjeubaoit <anhtn.bk@gmail.com>
2020-08-03 21:57:11 +0700

    leetcode 38

---
+++ leetcode/38.count-and-say.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=38 lang=python3
+#
+# [38] Count and Say
+#
+from typing import List, Dict
+import unittest
+from common import BaseTestCase
+
+# @lc code=start
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        pass
+        
+# @lc code=end
+class P18TestCase(BaseTestCase):
+
+    def test1(self):
+        pass
+
+    def test2(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
