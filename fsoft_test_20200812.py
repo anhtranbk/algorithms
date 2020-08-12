b7f6ecb3b914efdea1bf1225c4c31158ba87efc4
tjeubaoit <anhtn.bk@gmail.com>
2020-08-12 09:27:48 +0700

    fsoft test

---
+++ fsoft_test_20200812.py
@@ -0,0 +1,17 @@
+# Find the set of all integers a, b, c, d that satisfy the following conditions:
+# 0 <= a^3 + b^3 + c^3 + d^3 <= N
+# a^3 + b^3 = c^3 + d3
+# 0 <= a, b, c, d <= N
+
+def find_satisfy_sets(N):
+    pass
+
+
+def three_sum(target):
+    pass
+    
+        
+    
+if __name__ == '__main__':
+    s = set[tuple[str]]()
+    s.add('abc')
