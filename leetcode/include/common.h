54d00ea43b169afcef26d93fbc81eb559e367bcf
tjeubaoit <anhtn.bk@gmail.com>
2020-09-19 11:49:12 +0700

    add c header common

---
+++ leetcode/include/common.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void bin(long n) {
+    long i;
+    cout << "0";
+    for (i = 1 << 30; i > 0; i = i / 2) {
+        if ((n & i) != 0) {
+            cout << "1";
+        } else {
+            cout << "0";
+        }
+    }
+    cout << endl;
+}
