a03c6b2dfccd5bcbe00ec9b54abf9b61c6e80410
tjeubaoit <anhtn.bk@gmail.com>
2020-10-11 09:49:14 +0700

    leetcode 535 (draft)

---
+++ leetcode/535.encode-and-decode-tiny-url.rs
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=535 lang=rust
+ *
+ * [535] Encode and Decode TinyURL
+ */
+
+// @lc code=start
+struct Codec {
+	
+}
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Codec {
+    fn new() -> Self {
+        
+    }
+	
+    // Encodes a URL to a shortened URL.
+    fn encode(&self, longURL: String) -> String {
+        
+    }
+	
+    // Decodes a shortened URL to its original URL.
+    fn decode(&self, shortURL: String) -> String {
+        
+    }
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * let obj = Codec::new();
+ * let s: String = obj.encode(strs);
+ * let ans: VecVec<String> = obj.decode(s);
+ */
+// @lc code=end
+
