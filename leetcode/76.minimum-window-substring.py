65625f04097ee6d603a1f6ca96d4e83c9bb5413c
tjeubaoit <anhtn.bk@gmail.com>
2020-10-15 23:42:05 +0700

    leetcode 76

--- leetcode/76.minimum-window-substring.py
+++ leetcode/76.minimum-window-substring.py
@@ -4,13 +4,56 @@
 # [76] Minimum Window Substring
 #
 from common import BaseTestCase, unittest
-from typing import List
+from typing import List, Dict
 
 # 
 # @lc code=start
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        expected = [0]*58 # ord('z') - ord('A') = 58
+        # count the frequency of characters in t
+        for c in t:
+            self.inc_freq(expected, c)
         
+        i, j, missing = 0, 0, len(t)
+        actual = [0]*58
+        ans = ''
+        # we use two pointers i, j to indicate sliding window
+        # if all characters of t are included in current window we increse j until enough characters 
+        # or the end of the string is reached
+        # while all characters of t are still being included in current window we increase to find the minimum window
+        # repeat two steps
+        for j in range(len(s)):
+            c = s[j]
+            self.inc_freq(actual, c)
+            # if the number of frequence of a particular character is less than or equals to 
+            # what actually needed we decrease missing to 1
+            if self.get_freq(actual, c) <= self.get_freq(expected, c):
+                missing -= 1            
+
+            # increase i (the left size of the window) while missing = 0 to find the minimum window
+            while missing == 0 and i <= j:
+                c = s[i]
+                if self.get_freq(actual, c) == self.get_freq(expected, c):
+                    missing += 1
+                    l = j - i + 1
+                    if not ans or l < len(ans):
+                        ans = s[i:j+1]
+
+                self.dec_freq(actual, c)
+                i += 1
+
+        return ans
+    
+    def get_freq(self, freq: List[int], c: str) -> int:
+        return freq[ord(c) - ord('A')] 
+
+    def inc_freq(self, freq: List[int], c: str):
+        freq[ord(c) - ord('A')] += 1
+
+    def dec_freq(self, freq: List[int], c: str):
+        freq[ord(c) - ord('A')] -= 1
+                
         
 # @lc code=end
 class P76TestCase(BaseTestCase):
