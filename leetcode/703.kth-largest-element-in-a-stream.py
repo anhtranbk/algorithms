fba50f02d1902b5ca6b09864e77be8a50960dc19
tjeubaoit <anhtn.bk@gmail.com>
2020-12-31 21:21:04 +0700

    lc 703

---
+++ leetcode/703.kth-largest-element-in-a-stream.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=703 lang=python3
+#
+# [703] Kth Largest Element in a Stream
+#
+import heapq
+
+
+# @lc code=start
+class KthLargest:
+    def __init__(self, k, nums):
+        self.pool = nums
+        self.k = k
+        heapq.heapify(self.pool)
+        while len(self.pool) > k:
+            heapq.heappop(self.pool)
+
+    def add(self, val):
+        if len(self.pool) < self.k:
+            heapq.heappush(self.pool, val)
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+# @lc code=end
